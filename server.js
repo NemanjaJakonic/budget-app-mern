deab1138fd4a02d89e3da83d35920b0a8cd24946
NemanjaJakonic <nemanjajakonic@gmail.com>
2022-12-05 09:16:28 +0100

    path fix

--- server.js
+++ server.js
@@ -31,9 +31,7 @@ if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("app/client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "app", "client", "build", "index.html")
-    );
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
